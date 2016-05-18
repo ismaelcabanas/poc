61d6277c591bb55db210b5ae03ea8a0fd2d554db
Ismael Cabañas Garcia <icabanas@ext.vectorsf.com>
2016-05-18 19:00:48 +0200

    Creación del proyecto sprint-boot-rest-service

---
+++ spring-boot-rest-service/src/main/java/cabanas/garcia/ismael/poc/App.java
@@ -0,0 +1,13 @@
+package cabanas.garcia.ismael.poc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
